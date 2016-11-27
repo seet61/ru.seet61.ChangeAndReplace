0170ac010b995fd09936d53ed46e34c4cd74545e
seet61 <dmitry.arefyev@RST-BSS-041-NB.Megafon.ru>
2016-11-27 16:34:15 +0300

    add first menu

--- src/ru/seet61/ChangeAndReplace/Main.java
+++ src/ru/seet61/ChangeAndReplace/Main.java
@@ -1,4 +1,7 @@
 package ru.seet61.ChangeAndReplace;
+
+import java.io.Console;
+
 /**
  * Данный класс будет решать сразу две задачи:
  * При запуске будет предлагаться ывбор что из этого будем делать.
@@ -8,6 +11,44 @@ package ru.seet61.ChangeAndReplace;
  */
 public class Main {
     public static void main(String[] args) {
-        
+        //Переменные
+        String string;
+        int answer;
+
+        //Подключаемся к консоли.
+        Console c = System.console();
+        if (c == null) {
+            System.err.println("No console.");
+            System.exit(1);
+        }
+
+        System.out.println("Добро пожаловать!");
+        System.out.println("Доступны два варианта работы:");
+        System.out.println("1 - работа с элементами массива, 2 - замена всех вхождений в троку");
+
+        while (true) {
+            string = c.readLine("Выберете, пожалуйста, выриант: ");
+            if (string.length() == 1) {
+                answer = Integer.valueOf(string);
+                switch (answer) {
+                    case 1: workWithArray(c);
+                            break;
+                    case 2: workWithString(c);
+                            break;
+                    default:
+                            System.out.println("неверный параметр");
+                            break;
+
+                }
+            }
+        }
     }
+
+    private static void workWithString(Console c) {
+    }
+
+    private static void workWithArray(Console c) {
+    }
+
+
 }
